e948f7a391d884285a7884e047c7b76623c9475d
Andrew Backer <awbacker@gmail.com>
2018-05-08 18:45:55 +0800

    Add GetFarm view and fix data in CreateFarm

--- server/coffeechain/apps/farm/api/views.py
+++ server/coffeechain/apps/farm/api/views.py
@@ -3,7 +3,7 @@ from rest_framework.views import APIView
 
 from coffeechain.apps.farm.api.serializers import CreateFarmSerializer, AddCertSerializer
 from coffeechain.proto import address
-from coffeechain.proto.coffee_pb2 import Farm, Events
+from coffeechain.proto.coffee_pb2 import Farm, Events, CoffeeChainEvents, Certification
 from coffeechain.services import sawtooth_api
 from coffeechain.utils.drf.validation import validate_using
 
@@ -12,24 +12,19 @@ class CreateFarmView(APIView):
     def post(self, request, *args, **kwargs):
         data = validate_using(CreateFarmSerializer, data=request.data, view=self)
 
-        print(data)
-
         farm = Farm()
         farm.key = data['key']
         farm.name = data['name']
         farm.location.lat = data['location']['lat']
         farm.location.lng = data['location']['lng']
         farm.location.description = data['location']['description']
-
-        print("%r" % farm)
+        farm.certifications.extend(data.get('certifications', []))
 
         resp = sawtooth_api.submit_batch(
             sawtooth_api.create_txn(
-                obj=farm,
-                # need the certifications to exist
-                inputs=[address.for_cert(c) for c in data['certifications']],
-                # this is the address we will be writing to
-                outputs=[address.for_farm(data['key'])]
+                obj=CoffeeChainEvents(farm_create=farm),
+                inputs=[address.for_cert(c) for c in farm.certifications],
+                outputs=[address.for_farm(data['key'])]  # this is the address we will be writing to
             )
         )
 
@@ -49,3 +44,33 @@ class AddCertView(APIView):
         )
 
         return Response(data=resp)
+
+
+class GetFarmView(APIView):
+    def get(self, request, **kwargs):
+        farm_address = address.for_farm(kwargs.get('key', None))
+        err, farm = sawtooth_api.get_state_as(Farm, farm_address)
+
+        if err:
+            return Response(status=400, data={
+                "error": "Error getting Farm",
+                "details": {
+                    "error_code": err,
+                    "address": farm_address
+                }
+            })
+
+        data = sawtooth_api.proto_to_dict(farm)
+
+        def _get_cert(key):
+            cert_address = address.for_cert(key)
+            err, cert = sawtooth_api.get_state_as(Certification, cert_address)
+            if err:
+                cert = Certification(key=key, description="Certification does not exist")
+            return sawtooth_api.proto_to_dict(cert)
+
+        data['certifications'] = [_get_cert(c) for c in data['certifications']]
+
+        return Response(
+            data=data
+        )
--- server/coffeechain/apps/farm/urls.py
+++ server/coffeechain/apps/farm/urls.py
@@ -5,6 +5,7 @@ from coffeechain.apps.farm.api import views
 urlpatterns = [
     path("create/", views.CreateFarmView.as_view()),
     path("<slug:key>/", include([
+        path("", views.GetFarmView.as_view()),
         path("add-cert/", views.AddCertView.as_view())
     ]))
 ]
