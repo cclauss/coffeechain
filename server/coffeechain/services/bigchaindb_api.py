17d1b8072c2fb791c4b7242c0ae8952a011159a3
Shubhendu Shekhar <shubh.shekhar1991@gmail.com>
2018-11-02 10:24:22 +0100

    texted and fixed api methods

--- server/coffeechain/services/bigchaindb_api.py
+++ server/coffeechain/services/bigchaindb_api.py
@@ -2,19 +2,21 @@ from bigchaindb_driver import BigchainDB
 bdb_root_url = 'http://localhost:9984'
 bdb = BigchainDB(bdb_root_url)
 
-def create(data, metadata, issuer, recipient):
+def create(data, metadata, issuer_keypair, recipient_pub_key):
     tx = bdb.transactions.prepare(
             operation='CREATE', 
-            signers=issuer.public_key,
-            asset=data,
+            signers=issuer_keypair.public_key,
+            asset={
+                'data': data
+            },
             metadata=metadata,
-            recipients=recipient)
-    return sign_and_commit(tx, issuer)
+            recipients=recipient_pub_key)
+    return sign_and_send(tx, issuer_keypair)
 
-def transfer(asset_input_tx, metadata, owner, recipient):
+def transfer(asset_input_tx, metadata, owner_keypair, recipient_pub_key):
     output = asset_input_tx['outputs'][0]
     inputs_ = {
-    'fulfillment': output['condition']['details'],
+        'fulfillment': output['condition']['details'],
         'fulfills': {
         'output_index': 0,
         'transaction_id': asset_input_tx['id'],
@@ -26,15 +28,15 @@ def transfer(asset_input_tx, metadata, owner, recipient):
         inputs=inputs_,
         asset={'id': asset_input_tx['id']},
         metadata=metadata,
-        recipients=recipient)
-    return sign_and_commit(tx, owner)
+        recipients=recipient_pub_key)
+    return sign_and_send(tx, owner_keypair)
 
 def get_assets_by_user(public_key, spent=False):
     return bdb.outputs.get(public_key, spent)
 
 def get_asset(asset_id, operation=None):
-    return bdb.transactions.get(asset_id, operation)
+    return bdb.transactions.get(asset_id=asset_id, operation=operation)
 
-def sign_and_commit(tx, signer):
-    signed_tx = bdb.transactions.fulfill(tx, private_keys=signer.private_key)
+def sign_and_send(tx, signer):
+    signed_tx = bdb.transactions.fulfill(tx, signer.private_key)
     return bdb.transactions.send_commit(signed_tx)
